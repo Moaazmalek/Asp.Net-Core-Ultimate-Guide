2ea2bb75e55b7f36c6ce30927993d7a72f564d4c
moaaz malek <madhalfrwat@gmail.com>
2026-09-25 22:43:03 +0300

    Complete Partial View Section

---
+++ PartialViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViewsExample.Models;
+
+namespace PartialViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewData["ListTitle"] = "Cities";
+            ViewData["ListItems"] = new List<string>()
+            {
+                "Paris",
+                "New York",
+                "New Mumbai",
+                "Rome"
+            };
+            return View();
+        }
+        [Route("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+        [Route("programming-languages")]
+        public IActionResult ProgrammingLanguages()
+        {
+            ListModel listModel = new()
+            {
+                ListTitle = "Programming Languages",
+                ListItems = ["Python", "C#", "Go", "TypeScript"]
+            };
+            //return new PartialViewResult()
+            //{
+            //    ViewName = "_ListPartialView",
+            //    Model = listModel
+            //};
+            return PartialView("_ListPartialView", listModel);
+        }
+        
+    }
+}
---
+++ PartialViewsExample/Models/ListModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PartialViewsExample.Models
+{
+    public class ListModel
+    {
+        public string ListTitle { get; set; }
+        public List<string> ListItems { get; set; } = [];
+
+    }
+}
