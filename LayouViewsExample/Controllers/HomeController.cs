738833a289d91b9c073c11a3d0e8ec91e522ee71
moaaz malek <madhalfrwat@gmail.com>
2026-09-25 17:44:58 +0300

    Layout views section Completed.

---
+++ LayouViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayouViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("about-company")]
+        public IActionResult About()
+        {
+            return View();
+        }
+        [Route("contact-support")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ LayouViewsExample/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayouViewsExample.Controllers
+{
+    public class ProductsController : Controller
+    {
+        [Route("products")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //Url : /search-producs/1
+        [Route("search-products/{ProductID?}")]
+        public IActionResult Search(int? ProductID)
+        {
+            ViewBag.ProductID = ProductID;
+            return View();
+        }
+        [Route("order-products")]
+        public IActionResult Order()
+        {
+            return View();
+        }
+    }
+}
